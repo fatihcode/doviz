8733a3edc711eab46af171057bcccc4e71878336
Fatih <95652578+fatihqaz@users.noreply.github.com>
2022-01-25 17:53:39 +0300

    edit

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-// const url = "https://api.genelpara.com/embed/doviz.json"
-
 const url = "http://api.bigpara.hurriyet.com.tr/doviz/headerlist/anasayfa"
-
-const yerel = "data.json"
 
 const result = document.getElementById("result")
 const time = document.getElementById("time")
-
 const col = document.querySelectorAll(".col")
-
-console.log(col)
 
 async function getData(url) {
 
     const json = await fetch(url)
     const data = await json.json()
-
-    console.log(data)
-    console.log(data.data[3])
-    console.log(data.data[3].ALIS)
-    console.log((data.data[3].SEMBOL).substr(0, 3) + "/" + (data.data[3].SEMBOL).substr(3, 6))
-
-    console.log("ABC/XYZ")
-
-    console.log(`${data.data[6].YUZDEDEGISIM > 0 ? '<i class="bi bi-caret-up-fill"></i>' : '<i class="bi bi-caret-down-fill"></i>'}`)
-
 
     let veri = ""
 
@@ -46,9 +29,6 @@ async function getData(url) {
 
     result.innerHTML = veri
     time.innerHTML = `<i class="bi bi-clock"></i> Son Güncelleme: ${data.data[7].TARIH.substr(11,8)}`
-
 }
-
-
 
 getData(url)
